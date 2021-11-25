533ea3a308346c339ee8a539359155f5d0482ce5
0260000128 <0260000128@zwxict.com>
2021-11-25 16:52:03 +0800

    123

---
+++ src/FreeClassroomBean.java
@@ -0,0 +1,114 @@
+public class FreeClassroomBean {
+
+	//场地编号
+	private String cdbh;
+
+	//场地名称
+	private String cdmc;
+
+	//校区
+	private String xqmc;
+
+	//场地类别
+	private String cdlbmc;
+
+	//座位数
+	private String zws;
+
+	//考试座位数
+	private String kszws1;
+
+	//楼号
+	private String jxlmc;
+
+	//场地借用类型号
+	private String cdjylx;
+
+	//使用部门
+	private String sydxmc;
+
+	//托管部门
+	private String jgmc;
+
+	public String getCdbh() {
+		return cdbh;
+	}
+
+	public void setCdbh(String cdbh) {
+		this.cdbh = cdbh;
+	}
+
+	public String getCdmc() {
+		return cdmc;
+	}
+
+	public void setCdmc(String cdmc) {
+		this.cdmc = cdmc;
+	}
+
+	public String getXqmc() {
+		return xqmc;
+	}
+
+	public void setXqmc(String xqmc) {
+		this.xqmc = xqmc;
+	}
+
+	public String getCdlbmc() {
+		return cdlbmc;
+	}
+
+	public void setCdlbmc(String cdlbmc) {
+		this.cdlbmc = cdlbmc;
+	}
+
+	public String getZws() {
+		return zws;
+	}
+
+	public void setZws(String zws) {
+		this.zws = zws;
+	}
+
+	public String getKszws1() {
+		return kszws1;
+	}
+
+	public void setKszws1(String kszws1) {
+		this.kszws1 = kszws1;
+	}
+
+	public String getJxlmc() {
+		return jxlmc;
+	}
+
+	public void setJxlmc(String jxlmc) {
+		this.jxlmc = jxlmc;
+	}
+
+	public String getCdjylx() {
+		return cdjylx;
+	}
+
+	public void setCdjylx(String cdjylx) {
+		this.cdjylx = cdjylx;
+	}
+
+	public String getSydxmc() {
+		return sydxmc;
+	}
+
+	public void setSydxmc(String sydxmc) {
+		this.sydxmc = sydxmc;
+	}
+
+	public String getJgmc() {
+		return jgmc;
+	}
+
+	public void setJgmc(String jgmc) {
+		this.jgmc = jgmc;
+	}
+
+	
+}
